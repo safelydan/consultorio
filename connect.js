9a75083cbda85b190d11335ebc36c387d6449c69
daniel <daniellealuau@gmail.com>
2024-04-18 16:46:25 -0300

    dotenv

--- connect.js
+++ connect.js
@@ -1,6 +1,10 @@
 const Sequelize = require('sequelize');
+const dotenv = require('dotenv')
 
-const sequelize = new Sequelize('postgres', 'postgres', 'hyperballad', {
+dotenv.config()
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'postgres',
 
